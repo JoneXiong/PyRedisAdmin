9e5abdcd0c3267fe4fd384574d69bbace90d4379
jone@meilapp.com <jone@meilapp.com>
2015-11-30 17:39:51 +0800

    清理未用到的css、js

--- media/common/wsize.js
+++ media/common/wsize.js
@@ -1,8 +1,3 @@
-/*!
- * Copyright &copy; 2012-2013 <a href="https://github.com/thinkgem/jeesite">JeeSite</a> All rights reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- */
 
 // 主框架窗口大小调整
 $("#left").width(leftWidth);
